533d4b94befd160db72227091bc999f1a844932f
Odera5 <anikingsleyodera5@gmail.com>
2025-07-27 09:52:14 +0100

    last

--- index.js
+++ index.js
@@ -25,4 +25,20 @@ document.addEventListener("DOMContentLoaded", () => {
     hamburger.classList.toggle("open");
     hamburgerMenu.classList.toggle("visible");
   });
+
+  const reveals = document.querySelectorAll(".reveal");
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("show");
+          observer.unobserve(entry.target);
+        }
+      });
+    },
+    { threshold: 0.1 }
+  );
+  reveals.forEach((reveal) => {
+    observer.observe(reveal);
+  });
 });
